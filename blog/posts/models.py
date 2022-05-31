27cbd446c19a842d7e989e78a2bebc7756ed0903
Алексей Завадский <alesha.zawadsky@yandex.ru>
2022-05-31 13:20:37 +0300

    models admin views

---
+++ blog/posts/models.py
@@ -0,0 +1,26 @@
+from core.models import CreatedModel
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Post(CreatedModel):
+    text = models.TextField(
+        verbose_name='Текст поста',
+        help_text='Введите текст поста',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Пользователь',
+    )
+
+    def __str__(self):
+        return self.text[:15]
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
---
+++ blog/posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import Post
+
+
+def index(request):
+    template = 'posts/index.html'
+
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context=context)
